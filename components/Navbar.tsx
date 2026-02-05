457c0b5d81149a54e63a5741b3fd015b92e3bc73
surya <suryat6574@gmail.com>
2026-02-05 11:58:45 +0530

    cloud storage

---
+++ components/Navbar.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+import posthog from "posthog-js"
+
+const Navbar = () => {
+  const handleLogoClick = () => {
+    posthog.capture('logo_clicked')
+  }
+
+  const handleNavLinkClick = (linkName: string) => {
+    posthog.capture('nav_link_clicked', {
+      link_name: linkName,
+    })
+  }
+
+  return (
+    <header>
+        <nav>
+            <Link href='/' className="logo" onClick={handleLogoClick}>
+                <Image src="/icons/logo1.png" alt="logo" width={24} height={24}/>
+                <p>DevEvent</p>
+            </Link>
+            <ul>
+                <Link href="/" onClick={() => handleNavLinkClick('Home')}>Home</Link>
+                <Link href="/" onClick={() => handleNavLinkClick('Events')}>Events</Link>
+                <Link href="/" onClick={() => handleNavLinkClick('Create Event')}>Create Event</Link>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
